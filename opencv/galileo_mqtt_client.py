52bd387324c59591c4a96c952a27ca65eae23407
HaManhDong <dongha.27995@gmail.com>
2018-03-23 22:56:18 +0700

    fix faces detection to get one faces

--- opencv/galileo_mqtt_client.py
+++ opencv/galileo_mqtt_client.py
@@ -1,5 +1,35 @@
-import requests
+import paho.mqtt.client as mqtt
+import faces_detection
+import time
 
-if __name__ == '__main__':
-    r = requests.get('http://127.0.0.1:5000')
-    print(r.json())
+
+# The callback for when the client receives a CONNACK response from the server.
+def on_connect(client, userdata, flags, rc):
+    print("Connected with result code "+str(rc))
+
+    # Subscribing in on_connect() means that if we lose the connection and
+    # reconnect then subscriptions will be renewed.
+    client.subscribe("pas/mqtt/rfid/user_scan")
+
+# The callback for when a PUBLISH message is received from the server.
+def on_message(client, userdata, msg):
+    print(msg.topic+" - "+str(msg.payload))
+    if str(msg.payload) == "start_webcam":
+        faces_detection.main()
+        time.sleep(1)
+
+
+client = mqtt.Client()
+client.on_connect = on_connect
+client.on_message = on_message
+
+client.connect("10.42.0.85", 1883, 60)
+
+# Blocking call that processes network traffic, dispatches callbacks and
+# handles reconnecting.
+# Other loop*() functions are available that give a threaded interface and a
+# manual interface.
+# client.loop_forever()
+run = True
+while run:
+    client.loop(timeout=1.0)
--- opencv/galileo_requests.py
+++ opencv/galileo_requests.py
@@ -1,5 +1,8 @@
 import requests
+import os
 
-url = "http://localhost:8000"
-
-r = requests.post(url, files=files)
+# url = "http://localhost:8000/pas/api/upload-image/"
+#
+# files = {'face': open('faces/face_11.jpg', 'rb')}
+# r = requests.post(url,  files=files)
+print(os.path.join('/home/donghm/git/final/Final-Project/', 'opencv/faces/'))
